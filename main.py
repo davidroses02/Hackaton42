97d731e473f5d190c72ab69c202c1da334f9922e
davidroses02 <davidrosasalcaraz@gmail.com>
2023-04-22 15:38:02 +0200

    codigoBase

---
+++ main.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch()
+    page = browser.new_page()
+
+    page.goto('https://www.example.com')
+    title = page.title()
+    print(title)
+
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
+
+
